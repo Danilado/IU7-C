6e89655a786608656092f3e44d2ae4ff7a9f8d07
Zvyagin Daniil <zvyagindo@student.bmstu.ru>
2023-02-27 19:54:34 +0300

    lab_01_08_03 refactored

--- lab_01_08_03/main.c
+++ lab_01_08_03/main.c
@@ -1,32 +1,27 @@
+#include <inttypes.h>
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 
 #define EVERYTHING_OK 0
 #define INPUT_ERROR 1
-#define RUNTIME_ERROR 2
+
+// 01010101....
+#define ODD_BITS 1431655765
+// 10101010....
+#define EVEN_BITS 2863311530
 
 uint32_t encrypt(uint32_t num)
 {
-    uint32_t odd_bits, even_bits;
+    uint32_t odd_part, even_part;
 
-    // это не совсем костанты, чтобы не вводить
-    // ещё две переменные, я использую те, которые изначально
-    // обхявляю как маски. Я решил переименовать их, чтобы
-    // обосновать свою позицию.
+    odd_part = ODD_BITS & num;
+    even_part = EVEN_BITS & num;
 
-    // 01010101....
-    odd_bits = 1431655765;
-    // 10101010....
-    even_bits = 2863311530;
+    odd_part = odd_part << 1;
+    even_part = even_part >> 1;
 
-    odd_bits = odd_bits & num;
-    even_bits = even_bits & num;
-
-    odd_bits = odd_bits << 1;
-    even_bits = even_bits >> 1;
-
-    return odd_bits | even_bits;
+    return odd_part | even_part;
 }
 
 void print_bin(uint32_t num)
@@ -42,7 +37,7 @@ int main(void)
     uint32_t number, encrypted_num;
 
     printf("Введите число для шифрования: ");
-    if (scanf("%u", &number) != 1)
+    if (scanf("%" SCNu32, &number) != 1)
     {
         printf("Error: Неправильный ввод!\n");
         return INPUT_ERROR;
