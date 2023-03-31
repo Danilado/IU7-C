0c82cdb0afcbac1f1bf1da8568abfb8691dc2735
Zvyagin Daniil <zvyagindo@student.bmstu.ru>
2023-03-31 20:59:00 +0300

    lab_03_01_03 fix module

--- lab_03_01_03/matrixes.c
+++ lab_03_01_03/matrixes.c
@@ -2,13 +2,6 @@
 
 #include <stdlib.h>
 
-void int_swap(int *first, int *second)
-{
-    int *tmp = second;
-    *second = *first;
-    *first = *tmp;
-}
-
 size_t matrix_scan(matrix_t matrix, size_t rows, size_t columns)
 {
     size_t el_count = 0;
@@ -21,28 +14,4 @@ size_t matrix_scan(matrix_t matrix, size_t rows, size_t columns)
         }
 
     return el_count;
-}
-
-void matrix_print(matrix_t matrix, size_t rows, size_t columns)
-{
-    for (size_t i = 0; i < rows; ++i)
-    {
-        for (size_t j = 0; j < columns; ++j)
-            printf("%d ", matrix[i][j]);
-        printf("\n");
-    }
-}
-
-void transpond(matrix_t dstmat, matrix_t srcmat, size_t r, size_t c)
-{
-    for (size_t i = 0; i < r; ++i)
-        for (size_t j = 0; j < c; ++j)
-            dstmat[j][i] = srcmat[i][j];
-}
-
-void transpond_in_place(matrix_t matrix, size_t rows, size_t columns)
-{
-    for (size_t i = 0; i < rows; ++i)
-        for (size_t j = 0; j < columns; ++j)
-            int_swap(&matrix[i][j], &matrix[j][i]);
 }
--- lab_03_01_03/matrixes.h
+++ lab_03_01_03/matrixes.h
@@ -8,7 +8,3 @@
 typedef int matrix_t[ROWMAX][COLMAX];
 
 size_t matrix_scan(matrix_t matrix, size_t rows, size_t columns);
-void matrix_print(matrix_t matrix, size_t rows, size_t columns);
-void transpond(matrix_t dstmat, matrix_t srcmat, size_t r, size_t c);
-void transpond_in_place(matrix_t matrix, size_t rows, size_t columns);
-void int_swap(int *first, int *second);
