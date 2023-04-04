f3c91263630f4c38ca7247805d4b225e924af6a8
Zvyagin Daniil <zvyagindo@student.bmstu.ru>
2023-04-04 22:25:59 +0300

    fix 03_06_03

--- lab_03_06_03/main.c
+++ lab_03_06_03/main.c
@@ -13,22 +13,27 @@ void matrix_spiral_fill(matrix_t matrix, size_t rows, size_t columns)
     int addition = 0;
     for (size_t i = 0; i < rows / 2 + rows % 2; ++i)
     {
+        // Сохраняем кол-во элементов, записанных на следующих слоях
         if (i > 0)
             addition += (rows + columns - 4 * i + 2) * 2;
 
+        // clang-format off
         for (size_t j = i; j < rows - i; ++j)
         {
             matrix[j][i] = addition - i + j + 1;
             matrix[columns - j - 1][rows - i - 1] =
-            addition - i + j + columns - 4 * i + rows - 1;
+                addition + j + columns - 5 * i + rows - 1;
         }
 
         for (size_t j = i + 1; j < rows - i - 1; ++j)
         {
+            // 2 * rows + columns - 7i <- прошли три четверти спирали
             matrix[i][columns - j - 1] =
-            addition + (rows - 2 * i) * 2 + (columns - 2 * i) + j - 2;
-            matrix[rows - i - 1][j] = addition + rows - 2 * i + j;
+                addition + 2 * rows + columns - 7 * i + j - 2;
+            // rows - 2i <- прошли одну четверть спирали
+            matrix[rows - i - 1][j] = addition + rows - 3 * i + j;
         }
+        // clang-format on
     }
 }
 
