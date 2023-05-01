14874d2ecaa0e67db04fa6a2b7bdfb8bf34a7c02
Zvyagin Daniil <zvyagindo@student.bmstu.ru>
2023-05-01 17:01:37 +0300

    lab_04_01 done

---
+++ lab_04_01/my_string.c
@@ -0,0 +1,76 @@
+#include "my_string.h"
+
+char *my_strpbrk(const char *s1, const char *s2)
+{
+    for (char *pcur = (char *)s1; *pcur != '\0'; ++pcur)
+        for (char *scur = (char *)s2; *scur != '\0'; ++scur)
+            if (*pcur == *scur)
+                return pcur;
+
+    return NULL;
+}
+
+size_t my_strspn(const char *s1, const char *s2)
+{
+    size_t ans = 0;
+
+    for (char *pcur = (char *)s1; *pcur != '\0'; ++pcur)
+    {
+        bool sub_found = false;
+        for (char *scur = (char *)s2; *scur != '\0'; ++scur)
+            if (*pcur == *scur)
+            {
+                sub_found = true;
+                break;
+            }
+
+        if (sub_found)
+            ++ans;
+        else
+            return ans;
+    }
+
+    return ans;
+}
+
+size_t my_strcspn(const char *s1, const char *s2)
+{
+    size_t ans = 0;
+
+    for (char *pcur = (char *)s1; *pcur != '\0'; ++pcur)
+    {
+        bool sub_found = false;
+        for (char *scur = (char *)s2; *scur != '\0'; ++scur)
+            if (*pcur == *scur)
+            {
+                sub_found = true;
+                break;
+            }
+
+        if (sub_found)
+            return ans;
+        else
+            ++ans;
+    }
+
+    return ans;
+}
+
+char *my_strchr(const char *s, int c)
+{
+    for (char *pcur = (char *)s; pcur == s || *(pcur - 1) != '\0'; ++pcur)
+        if (*pcur == c)
+            return pcur;
+
+    return NULL;
+}
+
+char *my_strrchr(const char *s, int c)
+{
+    char *result_pointer = NULL;
+    for (char *pcur = (char *)s; pcur == s || *(pcur - 1) != '\0'; ++pcur)
+        if (*pcur == c)
+            result_pointer = pcur;
+
+    return result_pointer;
+}
