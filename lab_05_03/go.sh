c19d9750566a9e06518a103f822bccc65d19cece
Zvyagin Daniil <zvyagindo@student.bmstu.ru>
2023-06-02 00:19:30 +0300

    removed unnecessary build

--- lab_05_03/go.sh
+++ lab_05_03/go.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 bash ./clean.sh
-bash ./func_tests/scripts/build_translator.sh
 
 echo DEBUG BUILD
 bash ./build_debug.sh
@@ -12,7 +11,6 @@ bash ./collect_coverage.sh
 echo
 
 bash ./clean.sh
-bash ./func_tests/scripts/build_translator.sh
 
 echo ADDRESS SANITIZER
 bash ./build_debug_asan.sh
@@ -20,7 +18,6 @@ bash ./func_tests/scripts/func_tests.sh
 echo
 
 bash ./clean.sh
-bash ./func_tests/scripts/build_translator.sh
 
 echo MEMORY SANITIZER
 bash ./build_debug_msan.sh
@@ -28,7 +25,6 @@ bash ./func_tests/scripts/func_tests.sh
 echo
 
 bash ./clean.sh
-bash ./func_tests/scripts/build_translator.sh
 
 echo UB SANITIZER
 bash ./build_debug_ubsan.sh
